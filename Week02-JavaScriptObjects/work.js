eca4daf8c30e18f7b139fd4708c41a81ca52cfba
Your Name <you@example.com>
2018-04-10 20:26:06 -0700

    Did JavaScriptObjects assignment

--- Week02-JavaScriptObjects/work.js
+++ Week02-JavaScriptObjects/work.js
@@ -1,10 +1,49 @@
+#!/usr/bin/env node
+
 var person = {
     firstName: "Rasmus",
-    lastName: "Lindved"
+    lastName: "Lindved",
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
 }
 
+var calculator = {
+    operand01: -1,
+    operand02: -1,
+    add: function(){
+        return this.operand01 + this.operand02;
+    },
+    subtract: function(){
+        return this.operand01 - this.operand02;
+    }
+}
+
+calculator.multiply = function(){
+    return this.operand01 * this.operand02;
+}
+
+calculator.operand01 = person.firstName.length;
+calculator.operand02 = person.lastName.length;
+
+function divider(title){
+    console.log("=====================================");
+    console.log(title);
+    console.log("=====================================");
+}
+
+divider("Person");
 console.log("Firstname: " + person.firstName);
 console.log("Lastname: " + person.lastName);
+console.log("Fullname: " + person.fullName());
+
+divider("Calculator")
+
+console.log("operand01 = " + calculator.operand01);
+console.log("operand02 = " + calculator.operand02);
+console.log("Add: " + calculator.add());
+console.log("Substract: " + calculator.subtract());
+console.log("Multiply: " + calculator.multiply());
 
 
 
