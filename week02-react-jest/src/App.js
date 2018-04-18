04882bba3a14b4ffa609c8c4f59d8d85cd0a7f23
Your Name <you@example.com>
2018-04-17 21:51:46 -0700

    Finished Address Assignment

--- week02-react-jest/src/App.js
+++ week02-react-jest/src/App.js
@@ -1,37 +1,65 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
             file: 'unknown',
-            foo: 'waiting',
-            firstName: 'unknown'
+            firstName: 'unknown',
+            lastName: 'unknown',
+            street: 'unknown',
+            city: 'unknown',
+            state: 'unknown',
+            zip: 'unknown'
         };
     }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <p className="App-intro">First Name: {this.state.firstName}</p>
-        <p className="App-intro">File: {this.state.file}</p>
-          <button id='btnGetFile' onClick={this.getFile}>Get File</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">File: {this.state.file}</p>
+                <button id='btnGetFile' className='Btn' onClick={this.getFile}>Get File</button>
+                <br/>
+                <br/>
+                <div className='Card' align="center">
+                    <br/>
+                    <h2>Address Details</h2>
+                    <p className="App-intro"><strong>First Name:</strong> {this.state.firstName}</p>
+                    <p className="App-intro"><strong>Last Name:</strong> {this.state.lastName}</p>
+                    <p className="App-intro"><strong>Street:</strong> {this.state.street}</p>
+                    <p className="App-intro"><strong>City:</strong> {this.state.city}</p>
+                    <p className="App-intro"><strong>State:</strong> {this.state.state}</p>
+                    <p className="App-intro"><strong>Zip:</strong> {this.state.zip}</p>
+                    <br/>
+                </div>
+                <br/>
+                <button id='btnSetAddress' className='Btn' onClick={this.setAddress}>Set Address</button>
+            </div>
+        );
+    }
 
-  getFile = () => {
+    getFile = () => {
         console.log('getFile called.');
         this.setState({file: 'url-file.js'});
-        this.setState({firstName: 'Rasmus'});
-  }
+    };
+
+    setAddress = () => {
+        this.setState({
+            firstName: 'Rasmus',
+            lastName: 'Lindved',
+            street: 'Lake Hills Blvd.',
+            city: 'Bellevue',
+            state: 'Washington',
+            zip: '98008'
+        })
+    };
 }
 
 export default App;
--- week02-react-jest/src/App.test.js
+++ week02-react-jest/src/App.test.js
@@ -7,24 +7,101 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({adapter: new Adapter});
 
 describe('Jest Create React Tests', function () {
-    it('renders without crashing', () => {
+    it('Renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
 
-    it('renders and reads H1 text', () => {
+    it('Renders and reads H1 text', () => {
         const wrapper = shallow(<App />);
         const welcome = <h1 className="App-title">Welcome to React</h1>;
         expect(wrapper.contains(welcome)).toBe(true);
     });
 
-
-    it('renders state of File paragraph after button click', () => {
+    it('Renders state of File paragraph after button click', () => {
         const wrapper = shallow(<App />);
         const fileSign = <p className="App-intro">File: url-file.js</p>;
         wrapper.find('#btnGetFile').simulate('click');
         expect(wrapper.contains(fileSign)).toBe(true);
+    });
+
+    it('Renders and display the default first name', () => {
+        const wrapper = shallow(<App/>);
+        const defaultName = <p className="App-intro"><strong>First Name:</strong> unknown</p>
+        expect(wrapper.contains(defaultName)).toBe(true);
+    });
+
+    it('Renders state of FirstName paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const nameDisplayed = <p className="App-intro"><strong>First Name:</strong> Rasmus</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(nameDisplayed)).toBe(true);
+    });
+
+    it('Renders and display the default last name', () => {
+        const wrapper = shallow(<App/>);
+        const defaultName = <p className="App-intro"><strong>Last Name:</strong> unknown</p>
+        expect(wrapper.contains(defaultName)).toBe(true);
+    });
+
+    it('Renders state of LastName paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const nameDisplayed = <p className="App-intro"><strong>Last Name:</strong> Lindved</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(nameDisplayed)).toBe(true);
+    });
+
+    it('Renders and display the default street', () => {
+        const wrapper = shallow(<App/>);
+        const defaultStreet = <p className="App-intro"><strong>Street:</strong> unknown</p>
+        expect(wrapper.contains(defaultStreet)).toBe(true);
+    });
+
+    it('Renders state of Street paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const streetDisplayed = <p className="App-intro"><strong>Street:</strong> Lake Hills Blvd.</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(streetDisplayed)).toBe(true);
+    });
+
+    it('Renders and display the default city', () => {
+        const wrapper = shallow(<App/>);
+        const defaultCity = <p className="App-intro"><strong>City:</strong> unknown</p>
+        expect(wrapper.contains(defaultCity)).toBe(true);
+    });
+
+    it('Renders state of City paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const cityDisplayed = <p className="App-intro"><strong>City:</strong> Bellevue</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(cityDisplayed)).toBe(true);
+    });
+
+    it('Renders and display the default state', () => {
+        const wrapper = shallow(<App/>);
+        const defaultState = <p className="App-intro"><strong>State:</strong> unknown</p>
+        expect(wrapper.contains(defaultState)).toBe(true);
+    });
+
+    it('Renders state of State paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const stateDisplayed = <p className="App-intro"><strong>State:</strong> Washington</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(stateDisplayed)).toBe(true);
+    });
+
+    it('Renders and display the default zip', () => {
+        const wrapper = shallow(<App/>);
+        const defaultZip = <p className="App-intro"><strong>Zip:</strong> unknown</p>
+        expect(wrapper.contains(defaultZip)).toBe(true);
+    });
+
+    it('Renders state of Zip paragraph after button click', () => {
+        const wrapper = shallow(<App/>);
+        const zipDisplayed = <p className="App-intro"><strong>Zip:</strong> 98008</p>
+        wrapper.find('#btnSetAddress').simulate('click');
+        expect(wrapper.contains(zipDisplayed)).toBe(true);
     });
 });
 
