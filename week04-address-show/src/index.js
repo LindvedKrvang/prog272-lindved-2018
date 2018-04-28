22412213df7d86f72d351296572a5b6e96b85947
Your Name <you@example.com>
2018-04-27 20:27:36 -0700

    Added Header components + Fixed bug that would make npm test crash on startup

--- week04-address-show/src/index.js
+++ week04-address-show/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import Address from './components/Address';
 import registerServiceWorker from './registerServiceWorker';
+import Header from "./components/Header";
 
 
 ReactDOM.render(
     <div>
+        <Header/>
         <Address/>
     </div>,
     document.getElementById('root'));
