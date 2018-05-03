bb16aba6a65d7d5daeec19534c97079d4659e693
Your Name <you@example.com>
2018-05-03 14:47:24 -0700

    Chaged to a working IP address

--- Week05-ReactNativeStarter/App.js
+++ Week05-ReactNativeStarter/App.js
@@ -20,18 +20,19 @@ export default class App extends React.Component {
     fetchMicro = (event) => {
         // this.setState({hello: 'fetcher king'});
         const that = this;
-        const ip = 'ccalvert.com'; // 168.156.47.60
-        fetch('http://' + ip + ':30027/you-rang')
+        const ip = '10.12.254.142'; // 168.156.47.60
+        fetch('http://' + ip + ':30025/search')
             .then((response) => response.json())
             .then(function (result) {
                 this.collection = result.allData;
                 console.log(result);
                 that.setState({
-                    hello: 'qux success',
+                    hello: JSON.stringify(result),
                     // SET UP STATE FOR FIRST AND LAST NAMES
                     // FROM THE DATABASE
                 });
             }).catch(function (ex) {
+                console.log(ex);
             that.setState({hello: 'qux error'});
         });
         if (event) {
