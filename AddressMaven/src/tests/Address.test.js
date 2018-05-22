a88a5ccfef5fe647845edd513eb79b3137827ccb
Your Name <you@example.com>
2018-05-22 14:33:40 -0700

    Found a way to test Address

--- AddressMaven/src/tests/Address.test.js
+++ AddressMaven/src/tests/Address.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Address from '../components/Address';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MockServer from '../dal/mock/MockServer';
@@ -20,6 +20,24 @@ describe('Testing Address component', function() {
         ReactDOM.render(component, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+
+    it('test Next', function () {
+        const MUI = shallow(component);
+        const wrapper = MUI.dive();
+        // console.log(wrapper.debug());
+        console.log("Index before: " + wrapper.state().index);
+        wrapper.find("#btnNext").simulate("click");
+        console.log("Index after: " + wrapper.state().index);
+    })
+
+    it('test Prev', function () {
+        const MUI = shallow(component);
+        const wrapper = MUI.dive();
+        // console.log(wrapper.debug());
+        console.log("Prev Index before: " + wrapper.state().index);
+        wrapper.find("#btnPrev").simulate("click");
+        console.log("Prev Index after: " + wrapper.state().index);
+    })
 
     // *********************************************************
     // Needs integrations tests to test this class
