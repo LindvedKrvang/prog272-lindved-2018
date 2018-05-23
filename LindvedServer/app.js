e426cb643e99b96ed156690ec5368da563d1e8c1
Your Name <you@example.com>
2018-05-23 08:56:17 -0700

    Added prettier

--- LindvedServer/app.js
+++ LindvedServer/app.js
@@ -10,8 +10,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
     next();
 });
 
@@ -30,18 +33,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- LindvedServer/routes/index.js
+++ LindvedServer/routes/index.js
@@ -1,51 +1,48 @@
-
-const AddressManager = require("../src/managers/addressManager");
+const AddressManager = require('../src/managers/addressManager');
 const fs = require('fs');
 
 var express = require('express');
 var router = express.Router();
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lindved Server' });
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Lindved Server' });
 });
 
-router.get('/nextAddress/:currentIndex', function (req, res) {
-   const currentIndex = parseInt(req.params["currentIndex"]);
-   const result = AddressManager.getNextAddress(currentIndex);
-   res.send({
-       index: result.index,
-       address: result.address
-   })
+router.get('/nextAddress/:currentIndex', function(req, res) {
+    const currentIndex = parseInt(req.params['currentIndex']);
+    const result = AddressManager.getNextAddress(currentIndex);
+    res.send({
+        index: result.index,
+        address: result.address
+    });
 });
 
-router.get('/get-address-list', function (req, res) {
-    readFile(__dirname + '/address-list.json')
-        .then((json) => {
-            res.send(JSON.parse(json.result));
-        });
+router.get('/get-address-list', function(req, res) {
+    readFile(__dirname + '/address-list.json').then(json => {
+        res.send(JSON.parse(json.result));
+    });
 });
 
 function readFile(fileName) {
     'use strict';
-    return new Promise(function(resolve, reject){
-        fs.readFile(fileName, 'utf8', function (err, fileContents) {
-            if(err){
+    return new Promise(function(resolve, reject) {
+        fs.readFile(fileName, 'utf8', function(err, fileContents) {
+            if (err) {
                 reject(err);
             }
             resolve({
-                'result': fileContents
+                result: fileContents
             });
         });
     });
 }
 
-router.get('/test', function (req, res) {
-   res.json({
-       test: "Test",
-       moreTest: "Test"
-   })
+router.get('/test', function(req, res) {
+    res.json({
+        test: 'Test',
+        moreTest: 'Test'
+    });
 });
 
 module.exports = router;
