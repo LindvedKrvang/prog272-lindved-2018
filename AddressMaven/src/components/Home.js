9a8c584103c0fef7d1716330b5aea0c055be6c77
Your Name <you@example.com>
2018-06-14 10:11:00 -0700

    Updated HomePage

--- AddressMaven/src/components/Home.js
+++ AddressMaven/src/components/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import AtSignImg from '../assets/atSign.png';
 
 export default class Home extends Component {
 
     render(){
         return (
             <div align="center">
-                <h1>Home</h1>
+                <h1>Address Maven</h1>
+                <h3>Welcome</h3>
+                <p>This WebApp allows you to keep track of the addresses of senators in the United States.</p>
+                <p>To get started - Please sync your database by going to 'Init Database' through the menu in the upper left corner.</p>
+                <img src={AtSignImg} alt="At logo"/>
             </div>
         )
     }
